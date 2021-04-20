1d4e50651951678abd74f97d1e1e6a9ef5dc0747
Sai Keshari <saikeshari@gmail.com>
2021-04-20 15:52:04 +0530

    Details page improved

--- src/components/detailsPage.js
+++ src/components/detailsPage.js
@@ -1,7 +1,6 @@
 //Child component - Display the data
 import React from 'react';
 import Header from '../components/Header.js';
-
 import { useState ,useEffect } from "react";
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
@@ -14,11 +13,56 @@ import queryString from "query-string";
 import { sellerSpots } from '../actions/parking'
 import { Card, Button } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
-import axios from 'axios'
+import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '95vh',
+  },
+  image: {
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Details = (props) => {
    const { auth } = useSelector((state) => ({ ...state }));
   const { token } = auth;
+
+  const classes = useStyles();
 
   const history = useHistory(); 
   const [parkingObj, setParkingObj] = useState(props.location.state.parkingObj);
@@ -47,7 +91,14 @@ const Details = (props) => {
     //   },
     // });
   }
-    return (
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} >
+        <img src={`${process.env.REACT_APP_API}/spot/image/${parkingObj._id}`} />
+        </Grid>
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
       <Card style={{border:'none',marginTop:'4rem' }}>
         <Card.Body>
 
@@ -71,7 +122,35 @@ const Details = (props) => {
             Confirm Booking and Pay now</Button>
         </Card.Body>
       </Card>
-    );
+      </Grid>
+    </Grid>
+  );
+    // return (
+      // <Card style={{border:'none',marginTop:'4rem' }}>
+      //   <Card.Body>
+      //     <img src={`${process.env.REACT_APP_API}/spot/image/${parkingObj._id}`} />
+
+      //   <Card.Title style={{fontSize:'3rem'}}>
+      //       {parkingObj.title}
+      //   </Card.Title>
+      //   <Card.Text style={{fontSize:'1.5rem'}}>
+      //           Location : {parkingObj.location}
+      //           <br/>
+      //           {parkingObj.content}
+      //           <br/>
+      //           Price : {parkingObj.price}
+      //           <br/>
+      //           From : {date[0]}
+      //           <br/>
+      //           To : {date[1]}
+      //           <br/>
+      //           Slots : {parkingObj.slots}
+      //   </Card.Text>
+      //     <Button variant="primary" onClick={handleSubmit}>
+      //       Confirm Booking and Pay now</Button>
+      //   </Card.Body>
+      // </Card>
+    // );
 
 }
 
